62ace5031897233c781e01d28996e34af57f2163
soniadiwedi <112754761+soniadiwedi@users.noreply.github.com>
2023-06-23 19:26:29 +0530

    1

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,17 +1,19 @@
 const jwt=require("jsonwebtoken")
 const auth=(req,res,next)=>{
-    const token=req.headers.authorization.split(" ")[1]
-    if(token){
-        const decoded=jwt.verify(token,"golgappe")   
-        if(decoded){
-            req.body.userID=decoded.userID
-            next()
-        }else{
-            res.status(403).send({message:"Invalid Token"})
-        } 
-    }else{
-        return res.status(401).send({message:"Token is not provided"})
+    const token = req.headers.authorization?.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ message: "No token provided" });
+  }
+
+  jwt.verify(token, "apple", (err, user) => {
+    if (err) {
+      return res.status(403).json({ message: "Invalid token" });
     }
+
+    req.user = user;
+    next();
+  });
 }
 
 module.exports=auth
